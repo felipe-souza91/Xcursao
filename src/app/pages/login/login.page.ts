f181ffb6ab3ee5d78e8a1ebe9e62d47697cad8b0
felipe-souza91 <felipe_souza91@live.com>
2020-08-25 20:40:17 -0300

    Cadastro e Login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { IonSlide, IonSlides } from '@ionic/angular';
+import { IonSlide, IonSlides, LoadingController, ToastController } from '@ionic/angular';
 import { Keyboard } from '@ionic-native/keyboard/ngx';
+import { User } from 'src/app/interfaces/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -12,8 +14,14 @@ export class LoginPage implements OnInit {
   @ViewChild(IonSlides) slides :IonSlides;
   public roadPosition:number=0;
   public roadDifference:number=80;
+  public userLogin: User = {};
+  public userRegister: User = {};
+  private loading : any;
 
-  constructor(public keyboard:Keyboard) { }
+  constructor(public keyboard:Keyboard,
+              private loadingCtrl: LoadingController,
+              private toastCtrl: ToastController,
+              private authService: AuthService) { }
 
   ngOnInit() {  
     this.keyboard
@@ -28,6 +36,71 @@ export class LoginPage implements OnInit {
       this.roadPosition -= this.roadDifference;
     }
     
+  }
+
+  async login(){
+    await this.presentLoading();
+    try{
+      await this.authService.login(this.userLogin);
+    }catch(error){
+      let message:string;
+      switch(error.code){
+        case 'auth/email-already-in-use':
+        message = 'Email ja Cadastrado';
+        break;
+
+        case 'auth/invalid-email':
+        message = 'Email Invalido';
+        break;
+      }
+      console.error(error);
+      this.presentToast(message);
+    }finally{
+      this.loading.dismiss();
+    }
+
+
+  }
+
+  async register(){
+    await this.presentLoading();
+    try{
+      await this.authService.register(this.userRegister);
+    }catch(error){
+      let message:string;
+      switch(error.code){
+        case 'auth/email-already-in-use':
+        message = 'Email ja Cadastrado';
+        break;
+
+        case 'auth/invalid-email':
+        message = 'Email Invalido';
+        break;
+      }
+      console.error(error);
+      this.presentToast(message);
+    }finally{
+      this.loading.dismiss();
+    }
+    
+    
+  }
+
+  async presentLoading() {
+    const loading = await this.loadingCtrl.create({
+      cssClass: 'my-custom-class',
+      message: 'Aguarde...',
+      //duration: 2000
+    });
+    return this.loading.present();
+  }
+
+  async presentToast(message : string) {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,24 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+  // Your web app's Firebase configuration
+
+    apiKey: "AIzaSyDDMrOMp3AP3-of-oBSKfQ7UEqPdrC3Htc",
+    authDomain: "xcursao.firebaseapp.com",
+    databaseURL: "https://xcursao.firebaseio.com",
+    projectId: "xcursao",
+    storageBucket: "xcursao.appspot.com",
+    messagingSenderId: "407006208662",
+    appId: "1:407006208662:web:be8501d2b5b9474fca18b7",
+    measurementId: "G-9PT81C1N0P"
+
+  // Initialize Firebase
+ /* firebase.initializeApp(firebaseConfig);
+  firebase.analytics();*/
+
+  }
 };
 
 /*
