ac68b1d055b8b4881ebc1938360f8bae82ead974
univer30 <60783280+univer30@users.noreply.github.com>
2020-10-19 21:11:56 -0300

    Atualização
    Modificação na conexão

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,16 +6,14 @@ export const environment = {
   production: false,
   firebase: {
     // Your web app's Firebase configuration
-    
-    apiKey: "AIzaSyABfMCDxbhWX5V-piNtKWnDBTrPxSIWqdQ",
-    authDomain: "xcursaoviagens.firebaseapp.com",
-    databaseURL: "https://xcursaoviagens.firebaseio.com",
-    projectId: "xcursaoviagens",
-    storageBucket: "xcursaoviagens.appspot.com",
-    messagingSenderId: "83194349138",
-    appId: "1:83194349138:web:d209241e08d1599782818b",
-    measurementId: "G-J87ESMTL7C"
-
+    apiKey: "AIzaSyDDMrOMp3AP3-of-oBSKfQ7UEqPdrC3Htc",
+    authDomain: "xcursao.firebaseapp.com",
+    databaseURL: "https://xcursao.firebaseio.com",
+    projectId: "xcursao",
+    storageBucket: "xcursao.appspot.com",
+    messagingSenderId: "407006208662",
+    appId: "1:407006208662:web:be8501d2b5b9474fca18b7",
+    measurementId: "G-9PT81C1N0P"
     // Initialize Firebase
     /* firebase.initializeApp(firebaseConfig);
      firebase.analytics();*/
